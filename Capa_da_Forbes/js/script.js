0aeb72951eec49d0f9bf236d5611fe990b74e965
GitGranvita <138223898+GitGranvita@users.noreply.github.com>
2024-02-18 14:17:43 -0300

    Delete Capa_da_Forbes directory

--- Capa_da_Forbes/js/script.js
+++
@@ -1,8 +0,0 @@
-// Seu código jQuery aqui
-$(document).ready(function(){
-    $("article").hover(function(){
-        $(this).fadeTo("fast", 0.8);
-    }, function(){
-        $(this).fadeTo("fast", 1);
-    });
-});
